972f6bdc4502caf7c4db9f4ae3301e08b02b400e
Nooruldeen Alsaif <nooruldeen.alsiaf@gmail.com>
2018-08-09 22:24:47 -0400

    Step 1 - Adding Java library to supplement app module with jokes

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,13 +7,17 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import io.bigsoft.android.jokes.JokeTeller;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    JokeTeller mJokeTeller;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mJokeTeller = new JokeTeller();
     }
 
 
@@ -40,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, mJokeTeller.getJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
